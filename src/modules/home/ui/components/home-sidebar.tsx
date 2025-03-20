037556bddb163bf7957400535502c2154d6adb2f
WILLIDEVELOPER <wilsai1023@gmail.com>
2025-03-20 13:33:57 -0500

    feat: add initial components and setup for home page and UI library
    This commit introduces the foundational components and setup for the home page and UI library. Key changes include:
    - Added `HomePage`, `HomeLayout`, `HomeNavbar`, and `HomeSideBar` components for the home page structure.
    - Implemented various UI components such as `Button`, `Input`, `Skeleton`, `AspectRatio`, and `Sonner` for the UI library.
    - Updated `layout.tsx` to use `Inter` font instead of `Geist`.
    - Added utility functions and hooks like `cn` and `useIsMobile`.
    - Included a logo SVG and configured the `components.json` for shadcn/ui setup.

---
+++ src/modules/home/ui/components/home-sidebar.tsx
@@ -0,0 +1,16 @@
+import { Separator } from "@/components/ui/separator";
+import { Sidebar, SidebarContent } from "@/components/ui/sidebar";
+import { MainSection } from "@/modules/home/ui/components/main-section";
+import { PersonalSection } from "@/modules/home/ui/components/personal-section";
+
+export const HomeSideBar = () => {
+  return (
+    <Sidebar className="pt-16 z-40 border-none">
+      <SidebarContent className="bg-background">
+        <MainSection />
+        <Separator/>
+        <PersonalSection/>
+      </SidebarContent>
+    </Sidebar>
+  );
+};
---
+++ src/modules/home/ui/components/personal-section.tsx
@@ -0,0 +1,60 @@
+"use client";
+
+import {
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "@/components/ui/sidebar";
+import { HistoryIcon, ListVideoIcon, ThumbsUpIcon } from "lucide-react";
+import Link from "next/link";
+
+const items = [
+  {
+    title: "History",
+    url: "/playlists/history",
+    icon: HistoryIcon,
+    auth: true,
+  },
+  {
+    title: "Liked videos",
+    url: "/playlists/liked",
+    icon: ThumbsUpIcon,
+    auth: true,
+  },
+  {
+    title: "All playlists",
+    url: "/playlists",
+    icon: ListVideoIcon,
+    auth: true,
+  },
+];
+
+export const PersonalSection = () => {
+  return (
+    <SidebarGroup>
+      <SidebarGroupLabel>You</SidebarGroupLabel>
+      <SidebarGroupContent>
+        <SidebarMenu>
+          {items.map((item) => (
+            <SidebarMenuItem key={item.title}>
+              <SidebarMenuButton
+                tooltip={item.title}
+                asChild
+                isActive={false} // Todo: change to look at current pathname
+                onClick={() => {}} //Todo: do something on click
+              >
+                <Link href={item.url} className="flex items-center gap-4">
+                  <item.icon className="w-5 h-5" />
+                  <span className="text-sm ">{item.title}</span>
+                </Link>
+              </SidebarMenuButton>
+            </SidebarMenuItem>
+          ))}
+        </SidebarMenu>
+      </SidebarGroupContent>
+    </SidebarGroup>
+  );
+};
